fe92b7b2a1928cb19b60c15bcb0b5962f4886f90
Roberto Lucas <andler.dev@gmail.com>
2025-05-01 04:20:51 -0600

    fix: lintin and auto biome format in nest

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
-	getHello(): string {
+	public getHello(): string {
 		return 'Hello World!'
+	}
+	public getDate(): string {
+		return new Date().toISOString()
+	}
+	public getDateWithMessage(): string {
+		return `Hello World! ${new Date().toISOString()}`
 	}
 }
