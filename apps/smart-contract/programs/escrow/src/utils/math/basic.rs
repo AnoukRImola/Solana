b50139f68bce431d659dc98a156ea1c6e8a5067a
armandocodecr <totovip1464@gmail.com>
2025-05-07 16:08:39 -0600

    feat: Add event struct for publishing escrow information

---
+++ apps/smart-contract/programs/escrow/src/utils/math/basic.rs
@@ -0,0 +1,18 @@
+use crate::errors::EscrowError;
+
+pub struct BasicMath;
+
+pub trait BasicArithmetic {
+    fn safe_add(a: i128, b: i128) -> Result<i128, EscrowError>;
+    fn safe_sub(a: i128, b: i128) -> Result<i128, EscrowError>;
+}
+
+impl BasicArithmetic for BasicMath {
+    fn safe_add(a: i128, b: i128) -> Result<i128, EscrowError> {
+        a.checked_add(b).ok_or(EscrowError::Overflow)
+    }
+
+    fn safe_sub(a: i128, b: i128) -> Result<i128, EscrowError> {
+        a.checked_sub(b).ok_or(EscrowError::Underflow)
+    }
+}
