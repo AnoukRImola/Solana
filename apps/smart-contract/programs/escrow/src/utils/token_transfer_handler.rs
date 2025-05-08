94c46babb9bb6f8a0db0be9f88bfeb2c43281295
armandocodecr <totovip1464@gmail.com>
2025-05-08 17:13:45 -0600

    feat: Add token transfer handler utility functions

---
+++ apps/smart-contract/programs/escrow/src/utils/token_transfer_handler.rs
@@ -0,0 +1,32 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{TokenAccount, Transfer, transfer};
+use crate::errors::EscrowError;
+
+pub fn has_sufficient_balance(account: &TokenAccount, amount: u64) -> Result<()> {
+    if account.amount < amount {
+        return Err(EscrowError::EscrowBalanceNotEnoughToSendEarnings.into());
+    }
+    Ok(())
+}
+
+pub fn transfer_from_escrow<'info>(
+    amount: u64,
+    from: AccountInfo<'info>,
+    to: AccountInfo<'info>,
+    authority: AccountInfo<'info>,
+    token_program: AccountInfo<'info>,
+    authority_seeds: &[&[u8]],
+) -> Result<()> {
+    transfer(
+        CpiContext::new_with_signer(
+            token_program,
+            Transfer {
+                from,
+                to,
+                authority,
+            },
+            &[authority_seeds],
+        ),
+        amount,
+    )
+}
